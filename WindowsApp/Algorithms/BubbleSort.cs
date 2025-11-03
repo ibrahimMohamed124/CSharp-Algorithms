fd05b38a1422f4e9c2417985072d2e79e0e87823
ibrahimMohamed124 <ibrahimrs1234@gmail.com>
2025-11-03 21:21:42 +0200

    fix: removed .vs and ignored temporary files

---
+++ WindowsApp/Algorithms/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿namespace WindowsApp.Algorithms
+{
+    internal class BubbleSort
+    {
+        public static void Sort(int[] array)
+        {
+            int length = array.Length;
+            bool swapped;
+            for (int i = 0; i < length - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        // Swap array[j] and array[j + 1]
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                // If no two elements were swapped in the inner loop, then break
+                if (!swapped)
+                    break;
+            }
+        }
+    }
+}
---
+++ WindowsApp/Program.cs
@@ -0,0 +1,13 @@
+﻿Dictionary<int, string> students = new Dictionary<int, string>();
+
+students.Add(1, "Ibrahim");
+students.Add(2, "Keemo");
+students.Add(3, "Mohamed");
+students.Add(4, "Said");
+
+foreach (var (id, name) in students)
+{
+    Console.WriteLine($"ID: {id}, Name: {name}");
+}
+
+// Keep the console window open 
