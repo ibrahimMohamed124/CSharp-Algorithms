ac44a6f31982abffd307664a11574d5f29030128
ibrahimMohamed124 <ibrahimrs1234@gmail.com>
2025-11-04 07:41:24 +0200

    sum array elements algorithm added

---
+++ WindowsApp/Algorithms/Arrays/SumArrayElements.cs
@@ -0,0 +1,24 @@
+﻿
+namespace WindowsApp.Algorithms.Arrays
+{
+    class SumArrayElements
+    {
+        public static int SumOfElementsInArray(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+        public static void TestSumOfElementsInArray()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int result = SumOfElementsInArray(arr);
+            Console.WriteLine("The sum of array elements is: " + result);
+            // Expected output: The sum of array elements is: 15
+        }
+    }
+}
