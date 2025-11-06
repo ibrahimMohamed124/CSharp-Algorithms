35db97cc08f58c9abd629ed64cb341e6d7475de9
ibrahimMohamed124 <ibrahimrs1234@gmail.com>
2025-11-06 19:10:50 +0200

    check string is palindrome algorithm added

---
+++ WindowsApp/Algorithms/Check Palindrome/CheckPalindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsApp.Algorithms.Check_Palindrome
+{
+    class CheckPalindrome
+    {
+        public static bool isPalindrome(string str)
+        {
+            int left = 0;
+            int right = str.Length - 1;
+            while (left < right)
+            {
+                if (str[left] != str[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        public static void TestCheckPalindrome()
+        {
+            string str = "racecar";
+            bool result = isPalindrome(str);
+            Console.WriteLine($"Is '{str}' a palindrome? {result}");
+            // Expected output: Is 'racecar' a palindrome? True
+        }
+    }
+}
