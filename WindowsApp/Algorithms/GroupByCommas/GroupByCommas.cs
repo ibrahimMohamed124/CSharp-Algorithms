2825376ec4eb6ab461ed1168983400a5ba764ffa
ibrahimMohamed124 <ibrahimrs1234@gmail.com>
2025-11-14 20:09:19 +0200

    group numbers by commas algorithm added

---
+++ WindowsApp/Algorithms/GroupByCommas/GroupByCommas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsApp.Algorithms.GroupByCommas
+{
+    public class GroupByCommas
+    {
+        public static string GroupNumbers(int number)
+        {
+            string numberStr = number.ToString();
+
+            for (int i = numberStr.Length - 3; i > 0; i -= 3)
+            {
+                numberStr = numberStr.Insert(i, ",");
+            }
+
+            return numberStr;
+        }
+
+        public static void TestGroupNumbers()
+        {
+            int input = 1234567890;
+            string output = GroupNumbers(input);
+            Console.WriteLine("Input: " + input);
+            Console.WriteLine("Output: " + output);
+        }
+    }
+}
--- WindowsApp/Program.cs
+++ WindowsApp/Program.cs
@@ -10,36 +10,38 @@ using WindowsApp.Algorithms.Delegates;
 using WindowsApp.Algorithms.Reformatting;
 using WindowsApp.Algorithms.Scrumblies;
 using WindowsApp.Algorithms.StringOperations;
+using WindowsApp.Algorithms.GroupByCommas;
 
 namespace WindowsApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
             Console.WriteLine("==================== Welcome to my github repository ====================");
-            BubbleSort.TestBubbleSort(); // Added test call for BubbleSort
-            LargestNumberAlgorithm.TestLargestNumber(); // Added test call for LargestNumberAlgorithm
-            SecondLargestNumberAlgorithms.TestFindSecondLargest(); // Added test call for SecondLargestNumberAlgorithms
-            SmallestNumberAlgorithm.TestSmallestNumber(); // Added test call for SmallestNumberAlgorithm
-            FindMissingNumber.TestMissingNumber(); // Added test call for FindMissingNumber
-            SumArrayElements.TestSumOfElementsInArray(); // Added test call for SumArrayElements
-            CaeserEncryption.TestCaeserENC();  // Added test call for CaeserEncryption
-            CaeserDecryption.TestCaeserDEC(); // Added test call for CaeserDecryption
-            BinarySearch.TestBinarySearch(); // Added test call for BinarySearch
-            CheckPalindrome.TestCheckPalindrome(); // Added test call for CheckPalindrome
-            isAnagram.TestIsAnagram();  // Added test call for CheckAnagram
-            BruteForce.TestBruteForce(); // Added test call for BruteForce
-            VigENC.TestVigENC(); // Added test call for Vigenere Encryption
-            // VigDEC.TestVigDEC(); // Added test call for Vigenere Decryption // Currently Commented Out
-            ExtractEvens.TestGetEvenNumbers(); // Added test call for ExtractEvens
-            ExtractEvens.TestIsEven(); // Added test call for IsEven
-            DateReformatting.TestReformatDate(); // Added test call for reformat date
-            CheckArrayOrder.TestCheckArrayOrder(); // Added test call for chheck array order
-            Scrumblies.TestScrumble(); // Added test call for scramble
-            LinearSearching.TestLinearSearch(); // Added test call for linear search
-            ReverseString.TestReverseString(); // Added test for reverse string
-            RemoveDuplicateWords.TestRemoveDuplicates(); // Added test call for RemoveDuplicateWords
+            //BubbleSort.TestBubbleSort(); // Added test call for BubbleSort
+            //LargestNumberAlgorithm.TestLargestNumber(); // Added test call for LargestNumberAlgorithm
+            //SecondLargestNumberAlgorithms.TestFindSecondLargest(); // Added test call for SecondLargestNumberAlgorithms
+            //SmallestNumberAlgorithm.TestSmallestNumber(); // Added test call for SmallestNumberAlgorithm
+            //FindMissingNumber.TestMissingNumber(); // Added test call for FindMissingNumber
+            //SumArrayElements.TestSumOfElementsInArray(); // Added test call for SumArrayElements
+            //CaeserEncryption.TestCaeserENC();  // Added test call for CaeserEncryption
+            //CaeserDecryption.TestCaeserDEC(); // Added test call for CaeserDecryption
+            //BinarySearch.TestBinarySearch(); // Added test call for BinarySearch
+            //CheckPalindrome.TestCheckPalindrome(); // Added test call for CheckPalindrome
+            //isAnagram.TestIsAnagram();  // Added test call for CheckAnagram
+            //BruteForce.TestBruteForce(); // Added test call for BruteForce
+            //VigENC.TestVigENC(); // Added test call for Vigenere Encryption
+            //// VigDEC.TestVigDEC(); // Added test call for Vigenere Decryption // Currently Commented Out
+            //ExtractEvens.TestGetEvenNumbers(); // Added test call for ExtractEvens
+            //ExtractEvens.TestIsEven(); // Added test call for IsEven
+            //DateReformatting.TestReformatDate(); // Added test call for reformat date
+            //CheckArrayOrder.TestCheckArrayOrder(); // Added test call for chheck array order
+            //Scrumblies.TestScrumble(); // Added test call for scramble
+            //LinearSearching.TestLinearSearch(); // Added test call for linear search
+            //ReverseString.TestReverseString(); // Added test for reverse string
+            //RemoveDuplicateWords.TestRemoveDuplicates(); // Added test call for RemoveDuplicateWords
+            GroupByCommas.TestGroupNumbers(); // Added test call for GroupByCommas
         }
     }
 }
