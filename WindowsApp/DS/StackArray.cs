8967fbb19449c63fab9fa40790fe0339fb37aaf8
ibrahimMohamed124 <ibrahimrs1234@gmail.com>
2025-11-17 14:25:58 +0200

    stack array algorithm added

---
+++ WindowsApp/DS/StackArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsApp.DS
+{
+    class StackArray
+    {
+        public int[] array;
+        public int top;
+        public StackArray(int size)
+        {
+            array = new int[size];
+            top = -1;    
+        }
+
+        public void Push(int data)
+        {
+            if(top == array.Length - 1) {
+                Console.WriteLine("Stack Overflow");
+                return;
+            }
+            array[top] = data;
+        }   
+
+        public int Pop()
+        {
+            if(top <=0) {
+                Console.WriteLine("Stack Underflow");
+                return -1;
+            }
+            return array[top--];
+        } 
+    }
+}
