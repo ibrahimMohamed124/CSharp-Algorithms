a00d948fbc3445d7af3e06f9b0a813c405148635
ibrahimMohamed124 <ibrahimrs1234@gmail.com>
2025-11-17 11:59:40 +0200

    count number of each character in dictionary

---
+++ WindowsApp/Algorithms/StringOperations/AddTwoNumberWithStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsApp.Algorithms.StringOperations
+{
+    public class AddTwoNumberWithStrings
+    {
+        public static string Add(string a, string b)
+        {
+            double doubleA = double.Parse(a);
+            double doubleB = double.Parse(b);
+            double res = doubleA + doubleB;
+            return res.ToString();
+        }
+    }
+}
---
+++ WindowsApp/Algorithms/StringOperations/CountCharsinString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsApp.Algorithms.StringOperations
+{
+    public class CountCharsinString
+    {
+        public static Dictionary<char,int> Count(string str)
+        {
+             var result = new Dictionary<char, int>();
+
+        foreach (char c in str)
+        {
+            if (result.ContainsKey(c))
+                result[c]++;
+            else
+                result[c] = 1;
+        }
+
+        return result;
+        }
+    }
+}
