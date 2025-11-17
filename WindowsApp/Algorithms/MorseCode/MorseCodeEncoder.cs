f52d547fbe4f0af7bb32f02c87c2649025a202fb
ibrahimMohamed124 <ibrahimrs1234@gmail.com>
2025-11-17 13:15:24 +0200

    morse code encoder added

---
+++ WindowsApp/Algorithms/MorseCode/MorseCodeEncoder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsApp.Algorithms.MorseCode
+{
+    public class MorseCodeEncoder
+    {
+        public static StringBuilder Encode(string text) 
+        {
+            var MorseCodeDict = new Dictionary<string, string>
+            {
+                    // Letters
+                {".-", "A"}, {"-...", "B"}, {"-.-.", "C"}, {"-..", "D"},
+                {".", "E"}, {"..-.", "F"}, {"--.", "G"}, {"....", "H"},
+                {"..", "I"}, {".---", "J"}, {"-.-", "K"}, {".-..", "L"},
+                {"--", "M"}, {"-.", "N"}, {"---", "O"}, {".--.", "P"},
+                {"--.-", "Q"}, {".-.", "R"}, {"...", "S"}, {"-", "T"},
+                {"..-", "U"}, {"...-", "V"}, {".--", "W"}, {"-..-", "X"},
+                {"-.--", "Y"}, {"--..", "Z"},
+
+                // Numbers
+                {"-----", "0"}, {".----", "1"}, {"..---", "2"}, {"...--", "3"},
+                {"....-", "4"}, {".....", "5"}, {"-....", "6"}, {"--...", "7"},
+                {"---..", "8"}, {"----.", "9"},
+
+                // Special characters
+                {".-.-.-", "."}, {"--..--", ","}, {"..--..", "?"}, {".----.", "'"},
+                {"-.-.--", "!"}, {"-..-.", "/"}, {"-.--.", "("}, {"-.--.-", ")"},
+                {".-...", "&"}, {"---...", ":"}, {"-.-.-.", ";"}, {"-...-", "="},
+                {".-.-.", "+"}, {"-....-", "-"}, {"..--.-", "_"}, {".-..-.", "\""},
+                {"...-..-", "$"}, {".--.-.", "@"},
+
+                // Special service codes
+                {"...---...", "SOS"}
+            };
+
+            var encoded = new StringBuilder();
+            foreach (char c in text.ToUpper())
+            {
+                if (c == ' ')
+                {
+                    encoded.Append("   "); // 3 spaces between words
+                }
+                else
+                {
+                    var morseCode = MorseCodeDict.FirstOrDefault(x => x.Value == c.ToString()).Key;
+                    if (morseCode != null)
+                    {
+                        encoded.Append(morseCode + " "); // 1 space between letters
+                    }
+                }
+            }
+            return encoded;
+        }
+    }
+}
--- WindowsApp/Algorithms/StringOperations/CountCharsinString.cs
+++ WindowsApp/Algorithms/StringOperations/CountCharsinString.cs
@@ -8,19 +8,39 @@ namespace WindowsApp.Algorithms.StringOperations
 {
     public class CountCharsinString
     {
-        public static Dictionary<char,int> Count(string str)
+        public static Dictionary<char, int> Count(string str)
         {
-             var result = new Dictionary<char, int>();
+            var result = new Dictionary<char, int>();
 
-        foreach (char c in str)
-        {
-            if (result.ContainsKey(c))
-                result[c]++;
-            else
-                result[c] = 1;
+            foreach (char c in str)
+            {
+                if (result.ContainsKey(c))
+                    result[c]++; // value increment
+                else
+                    result[c] = 1; // initial value
+            }
+
+            return result;
         }
 
-        return result;
+    public static void TestCountChars()
+        {
+            string str = "hello world";
+            var charCount = CountCharsinString.Count(str);
+            Console.WriteLine($"Character counts in '{str}':");
+            foreach (var kvp in charCount)
+            {
+                Console.WriteLine($"'{kvp.Key}': {kvp.Value}");
+            }
+            // Expected output:
+            // 'h': 1
+            // 'e': 1
+            // 'l': 3
+            // 'o': 2
+            // ' ': 1
+            // 'w': 1
+            // 'r': 1
+            // 'd': 1
         }
     }
 }
